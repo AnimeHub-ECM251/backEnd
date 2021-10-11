97ab89732aa29cd27cf4f27b4c8da5ac958f9a3c
Bruno Vilardi <brunovilardibueno@gmail.com>
2021-10-10 21:02:05 -0300

    CRUD de mysql

---
+++ src/main/kotlin/repositories/errors/errors.kt
@@ -0,0 +1,5 @@
+package repositories.errors
+
+import java.lang.Exception
+
+class DATABASE_CONNECTION_FAILED: Exception("Data base connection failed")
---
+++ src/main/kotlin/repositories/mysql/repoMysql.kt
@@ -0,0 +1,75 @@
+package repositories.mysql
+
+import repositories.errors.DATABASE_CONNECTION_FAILED
+import util.DBTablePrinter
+import java.sql.*
+
+class RepoMysql (dbName: String = "animeHubDB"){
+    private val connection: Connection
+    private val SQLStatement: Statement
+    private val DBName: String
+
+
+    init {
+        this.connection = createConnection()
+        this.SQLStatement = createStatement()
+        this.DBName = dbName
+    }
+
+
+    private fun createConnection(): Connection {
+        try {
+            val c  = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306",
+                "root",
+                "password"
+            )
+            println("Connection created")
+            return c
+        }
+        catch (e: SQLException){
+            throw DATABASE_CONNECTION_FAILED()
+        }
+    }
+
+    private fun createStatement(): Statement {
+        return this.connection!!.createStatement()
+    }
+
+    private fun executeStatement(query: String) {
+        try {
+            val preparedStatement = this.connection!!.prepareStatement(query)
+            preparedStatement!!.executeUpdate()
+        }
+        catch (e: SQLException){
+            println("Not possible to execute update: ${e.message}")
+        }
+
+    }
+
+
+    fun read(table: String, where: String="TRUE"){
+        var result: ResultSet = this.SQLStatement.executeQuery("SELECT * FROM ${this.DBName}.${table} WHERE ${where}")
+        DBTablePrinter.printResultSet(result)
+    }
+
+    fun create(table: String, data: HashMap<String, String>){
+        var query = "INSERT INTO ${this.DBName}.${table} (${data.keys.joinToString { it -> "${it}" }}) values (${data.values.joinToString {it -> "\'${it}\'" }});"
+        executeStatement(query)
+
+
+    }
+
+    fun update(table: String, data: HashMap<String, String>){
+        val id = data.remove("id")
+        var query = "UPDATE ${this.DBName}.${table} SET ${data.keys.joinToString { it -> "${it} = \'${data[it]}\'" }} WHERE id = ${id}"
+        executeStatement(query)
+    }
+
+    fun delete(table: String, id: Int){
+        var query = "DELETE FROM ${this.DBName}.${table} WHERE id = ${id}"
+        executeStatement(query)
+    }
+
+
+}
