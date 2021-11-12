3ac02bcd6ce099cc565cdb1d033ad68edc2c5fd0
Bruno Vilardi <brunovilardibueno@gmail.com>
2021-11-12 11:18:25 -0300

    tentar conexao com db até conseguir

--- src/main/kotlin/presenter/Application.kt
+++ src/main/kotlin/presenter/Application.kt
@@ -9,12 +9,30 @@ import repositories.mysql.RepoMysql
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.http.HttpHeaders.AccessControlAllowHeaders
+import kotlinx.coroutines.delay
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 fun main() {
-    println("Servidor sendo iniciado!!!!@!!!!!!!")
+    println("Servidor sendo iniciado!!!!!!!!!!!")
 
-    val repo : IRepo = RepoMysql()
+    var repo : IRepo
+
+    while (true) {
+        try {
+            repo = RepoMysql()
+            break;
+        } catch (e : Exception) {
+            println("Não foi possível se conectar com o DB: ${e}")
+            println("Tentando conexão novamente em 5 segundos...")
+            Timer().schedule(5000) {
+                println("Tentando novamente...")
+            }
+        }
+        }
+
+
 
     embeddedServer(Netty, port = 8081, host = "0.0.0.0") {
         install(CORS) {
